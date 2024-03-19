a65c2a3d0be6f319918120be13e04bbf26492749
unknown <e.vanlopik@tilburguniversity.edu>
2024-03-19 16:28:35 +0100

    File 2 is done

--- src/2.Downloading_Inside_Airbnb_dataset_and_creating_France_Greece_dataset.R
+++ src/2.Downloading_Inside_Airbnb_dataset_and_creating_France_Greece_dataset.R
@@ -1,13 +1,19 @@
-#Load required libraries
+# The "Inside_Airbnb_URL_Dataset.csv" should be in the repository, check to make sure
+
+# First get your Inside_Airbnb_URL_Dataset by setting the working directory
+# Should look something like this: setwd("C/...The_impact_of_Airbnb_hosts_on_guest_reviews")
+setwd("C:/Users/HP/OneDrive/Documenten/M Marketing Analytics/Skills Data Preparation & Workflow Management/It support Julian/The_impact_of_Airbnb_hosts_on_guest_reviews")
+
+# Load required packages
 library(readr)
 library(dplyr)
 library(tidyverse)
 library(data.table)
 
-#We want to select the variables that are used in our dataset, hence we need to wrangle the data.
+# Wrangling the data
 
-#First we have to create a function that creates an url variable from the webscraped countries, regions and urls
-#Create a function to extract the url from the scraped url dataset and call it 'get_region_url'
+# Creating a function that creates an url variable from the webscraper including country, region and url
+# Function to extract the url from the scraped url dataset and call it 'get_region_url'
 get_region_url <- function(region_name) {
   #Save the read dataset under a variable
   url_data <- read.csv("Inside_Airbnb_URL_Dataset.csv")
@@ -30,13 +36,14 @@ url_Athens <- get_region_url("Athens")
 url_Crete <- get_region_url("Crete")
 url_South_Aegean <- get_region_url("South Aegean")
 url_thessaloniki <- get_region_url("Thessaloniki")
+
 #France
 url_Bordeaux <- get_region_url("Bordeaux")
 url_Lyon <- get_region_url("Lyon")
 url_Paris <- get_region_url("Paris")
 url_Pays_Basque <- get_region_url("Pays Basque")
 
-#Second we create a function that wrangles the dataset to our selected regions and returns the selected data
+# Creating a function that wrangles the dataset to our selected regions and returns the selected data
 download_and_process_selected_airbnb_data <- function(url, region, country) {
   # Creating a variable of the URL code
   file_name <- paste(region, "_data.csv", sep = "")
@@ -67,25 +74,36 @@ download_and_process_selected_airbnb_data <- function(url, region, country) {
   return(selected_data)
 }
 
-#Implementing the function to create datasets of the needed regions
-#Greece
+# Implementing the function to create datasets of the needed regions
+# Greece
 athens_selected_data <- download_and_process_selected_airbnb_data(url_Athens, "Athens", "Greece")
 crete_selected_data <- download_and_process_selected_airbnb_data(url_Crete, "Crete", "Greece")
 south_aegean_selected_data <- download_and_process_selected_airbnb_data(url_South_Aegean, "South Aegean", "Greece")
 thessaloniki_selected_data <- download_and_process_selected_airbnb_data(url_thessaloniki, "Thessaloniki", "Greece")
-#France
+
+# France
 bordeaux_selected_data <- download_and_process_selected_airbnb_data(url_Bordeaux, "Bordeaux", "France")
 lyon_selected_data <- download_and_process_selected_airbnb_data(url_Lyon, "Lyon", "France")
 paris_selected_data <- download_and_process_selected_airbnb_data(url_Paris, "Paris", "France")
 pays_basque_selected_data <- download_and_process_selected_airbnb_data(url_Pays_Basque, "Pays Basque", "France")
 
+# Combining all the selected regions of Greece and France together to make a dataset called 'France_Greece_selected_data' in csv form
+Inside_Airbnb_Final_Dataset <- bind_rows(athens_selected_data, crete_selected_data, south_aegean_selected_data, thessaloniki_selected_data, bordeaux_selected_data, lyon_selected_data, paris_selected_data, pays_basque_selected_data)
+write.csv(Inside_Airbnb_Final_Dataset, "Inside_Airbnb_Final_Dataset.csv", row.names = TRUE)
+View(Inside_Airbnb_Final_Dataset)
 
-#Third we combine all the selected regions of Greece and France together to make a dataset called 'France_Greece_selected_data' in csv form
-France_Greece_selected_data <- bind_rows(athens_selected_data, crete_selected_data, south_aegean_selected_data, thessaloniki_selected_data, bordeaux_selected_data, lyon_selected_data, paris_selected_data, pays_basque_selected_data)
-write.csv(France_Greece_selected_data, "France_Greece_selected_data.csv", row.names = TRUE)
-View(France_Greece_selected_data)
+# Recoding some variables so the dataset is ready for the next steps
+# Encoding the variables profile picture, identity verified and country as dummy variables
+Inside_Airbnb_Final_Dataset <- within(Inside_Airbnb_Final_Dataset, {
+  host_has_profile_pic <- as.numeric(host_has_profile_pic == "TRUE") #true=1, false=0
+  host_identity_verified <- as.numeric(host_identity_verified == "TRUE") #true=1, false=0
+  Country_Dataset <- as.numeric(Country_Dataset == "Greece") #Greece=1
+})
 
-#De functie van Rolf en Julian dient gecombineerd te worden.
-#Optimalisatie mogelijk bij stap 3. Dit zou een functie kunnen zijn, zodat het geschikt is voor meerdere regios die gecombineerd worden.
-#Alle functies kunnen in een totale functie gezet worden. Zodat bij ingave van alle regio's het definitieve csv bestand gemaakt wordt.
-#Reden: snelheid en optimalisatie automatisering. 
+# Encoding the dummy variables
+Inside_Airbnb_Final_Dataset$Country_Dataset <- ifelse(Inside_Airbnb_Final_Dataset$Country_Dataset == 1, "Greece", "France")
+Inside_Airbnb_Final_Dataset$host_has_profile_pic <- ifelse(Inside_Airbnb_Final_Dataset$host_has_profile_pic == 1, "Profile_picture", "NO_profile picture")
+Inside_Airbnb_Final_Dataset$host_identity_verified <- ifelse(Inside_Airbnb_Final_Dataset$host_identity_verified == 1, "ID_verified", "ID_NOT verified")
+
+# Check whether the variables are encoded the right way
+View(Inside_Airbnb_Final_Dataset)
