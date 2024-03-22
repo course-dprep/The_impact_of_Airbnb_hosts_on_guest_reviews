91316e4b4afd35c6d208fc882c5d34e8bb2953d1
Julian Peters <j.a.m.peters@outlook.com>
2024-03-22 14:06:55 +0100

    Finished the makefiles

--- src/1.Data-preparation/2.Download_Inside_Airbnb_Data_And_Create_France_Greece_Dataset.R
+++ src/1.Data-preparation/2.Download_Inside_Airbnb_Data_And_Create_France_Greece_Dataset.R
@@ -43,9 +43,9 @@ url_Pays_Basque <- get_region_url("Pays Basque")
 
 # Creating a place to save the files
 dir.create('Gen')
-dir.create('Gen/data-preparation')
-dir.create('Gen/data-preparation/input')
-dir.create('Gen/data-preparation/output')
+dir.create('Gen/1.Data-preparation')
+dir.create('Gen/1.Data-preparation/input')
+dir.create('Gen/1.Data-preparation/output')
 
 # Function to extract the url from the scraped url dataset and call it 'get_region_url'
 get_region_url <- function(region_name) {
@@ -67,7 +67,7 @@ get_region_url <- function(region_name) {
 # Creating a function that wrangles the dataset to our selected regions and returns the selected data
 Create_Region_Dataset <- function(url, region, country) {
   # Downloading the needed dataset from the url and saving it to our wanted file
-  file_name <- paste0("Gen/data-preparation/input/", region, ".csv")
+  file_name <- paste0("Gen/1.Data-preparation/input/", region, ".csv")
   if (!file.exists(file_name)) {
     message("Downloading file from ", url)
     tryCatch(download.file(url, file_name, mode = "wb"), 
@@ -86,7 +86,7 @@ Create_Region_Dataset <- function(url, region, country) {
     mutate(Region_Dataset = region, Country_Dataset = country)
   
   # Define a new filename for the modified dataset and saving location
-  modified_file_name <- paste0("Gen/data-preparation/output/", region, "_selected.csv")
+  modified_file_name <- paste0("Gen/1.Data-preparation/output/", region, "_selected.csv")
   
   # Add this new selection to the dataset
   message("Writing to CSV file...")
@@ -99,32 +99,6 @@ Create_Region_Dataset <- function(url, region, country) {
 }
 
 # Greece
-url_Athens <- get_region_url("Athens")
-url_Crete <- get_region_url("Crete")
-url_South_Aegean <- get_region_url("South Aegean")
-url_thessaloniki <- get_region_url("Thessaloniki")
-
-athens_selected_data <- Create_Region_Dataset(url_Athens, "Athens", "Greece")
-crete_selected_data <- Create_Region_Dataset(url_Crete, "Crete", "Greece")
-south_aegean_selected_data <- Create_Region_Dataset(url_South_Aegean, "South Aegean", "Greece")
-thessaloniki_selected_data <- Create_Region_Dataset(url_thessaloniki, "Thessaloniki", "Greece")
-
-# France
-url_Bordeaux <- get_region_url("Bordeaux")
-url_Lyon <- get_region_url("Lyon")
-url_Paris <- get_region_url("Paris")
-url_Pays_Basque <- get_region_url("Pays Basque")
-
-bordeaux_selected_data <- Create_Region_Dataset(url_Bordeaux, "Bordeaux", "France")
-lyon_selected_data <- Create_Region_Dataset(url_Lyon, "Lyon", "France")
-paris_selected_data <- Create_Region_Dataset(url_Paris, "Paris", "France")
-pays_basque_selected_data <- Create_Region_Dataset(url_Pays_Basque, "Pays Basque", "France")
-
-# Combining all the selected regions of Greece and France together to make a dataset called 'France_Greece_selected_data' in csv form
-Inside_Airbnb_Final_Dataset <- bind_rows(athens_selected_data, crete_selected_data, south_aegean_selected_data, thessaloniki_selected_data, bordeaux_selected_data, lyon_selected_data, paris_selected_data, pays_basque_selected_data)
-write.csv(Inside_Airbnb_Final_Dataset, "Gen/data-preparation/output/Inside_Airbnb_Final_Dataset.csv", row.names = FALSE)
-
-# Greece
 athens_selected_data <- Create_Region_Dataset(url_Athens, "Athens", "Greece")
 crete_selected_data <- Create_Region_Dataset(url_Crete, "Crete", "Greece")
 south_aegean_selected_data <- Create_Region_Dataset(url_South_Aegean, "South Aegean", "Greece")
@@ -138,5 +112,4 @@ pays_basque_selected_data <- Create_Region_Dataset(url_Pays_Basque, "Pays Basque
 
 # Combining all the selected regions of Greece and France together to make a dataset called 'France_Greece_selected_data' in csv form
 Inside_Airbnb_Final_Dataset <- bind_rows(athens_selected_data, crete_selected_data, south_aegean_selected_data, thessaloniki_selected_data, bordeaux_selected_data, lyon_selected_data, paris_selected_data, pays_basque_selected_data)
-final_dataset_file <- "Gen/data-preparation/output/Inside_Airbnb_Final_Dataset.csv"
-write.csv(Inside_Airbnb_Final_Dataset, final_dataset_file, row.names = TRUE)
+write.csv(Inside_Airbnb_Final_Dataset, "Gen/1.Data-preparation/output/Inside_Airbnb_Final_Dataset.csv", row.names = FALSE)
